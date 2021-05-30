5f3d7ea5bea358c88aa9e2b330fb19d8c25886b1
chris <chris@bumblehead.com>
2021-05-30 02:30:25 -0700

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,38 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch
+} from "react-router-dom";
+
 import Thai from "./Thai";
 
+// react-spa-prerender
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <StrictMode>
-    <Thai />
+    <BrowserRouter>
+      <nav>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="/user/:id">User</Link>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/user">
+          <div>user</div>
+        </Route>
+        <Route path="/" exact>
+          <Thai />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </StrictMode>,
   rootElement
 );
