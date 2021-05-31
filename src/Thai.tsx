94faeffdb3089bba8dc371d29d90b3ab77ac92e4
chris <chris@bumblehead.com>
2021-05-30 22:59:14 -0700

    add lint

--- src/Thai.tsx
+++ src/Thai.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 
 import ThaiNavPrimary from './ThaiNavPrimary';
 
-const Thai = () => {
+const Thai: React.FC = () => {
   return (
     <div>
       <ThaiNavPrimary />
--- src/i18n.ts
+++ src/i18n.ts
@@ -4,23 +4,24 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
-    en: {
-        translation: {
-            home: 'home',
-            contact: 'contact'
-        }
+  en: {
+    translation: {
+      home: 'home',
+      contact: 'contact'
     }
+  }
 };
 
-i18n.use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        resources,
-        fallbackLng: 'en',
-        debug: true,
-        interpolation: {
-            escapeValue: false
-        }
-    });
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
 export default i18n;
